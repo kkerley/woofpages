ac78f769947f152992376a3ed771eb8e2c14c0b2
Kyle Kerley <kyle.l.kerley@gmail.com>
2017-04-25 07:53:20 -0600

    Made some ASP changes and added a new template for the global dog search page

---
+++ wp-content/themes/FoundationPress-top-level-child/page-templates/global-search.php
@@ -0,0 +1,41 @@
+<?php
+/*
+Template Name: Global Dog Search
+*/
+get_header(); ?>
+
+<?php get_template_part( 'template-parts/featured-image' ); ?>
+
+<div id="page" role="main">
+<?php do_action( 'foundationpress_before_content' ); ?>
+<?php while ( have_posts() ) : the_post(); ?>
+    <article class="main-content">
+        <header>
+            <h1><?php the_title(); ?></h1>
+        </header>
+
+
+        <div class="entry-content">
+		    <?php the_content(); ?>
+		    <?php # edit_post_link( __( 'Edit', 'foundationpress' ), '<span class="edit-link">', '</span>' ); ?>
+        </div>
+
+
+
+       <section class="wrapper--global-dog-search">
+           <header>
+               <?php echo do_shortcode('[wd_asp id="1"]'); ?>
+           </header>
+
+           <footer>
+	           <?php echo do_shortcode('[wpdreams_ajaxsearchpro_results id="1" element="div"]'); ?>
+           </footer>
+       </section>
+
+    </article>
+<?php endwhile;?>
+    <?php get_sidebar(); ?>
+
+</div>
+
+<?php get_footer();
