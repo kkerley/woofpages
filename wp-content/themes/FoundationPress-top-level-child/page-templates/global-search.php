4f438a276e95b6253bf753a56764403966b746b9
Kyle Kerley <kyle.l.kerley@gmail.com>
2017-04-26 05:51:13 -0600

    Global search tweaks

--- wp-content/themes/FoundationPress-top-level-child/page-templates/global-search.php
+++ wp-content/themes/FoundationPress-top-level-child/page-templates/global-search.php
@@ -24,11 +24,11 @@ get_header(); ?>
 
        <section class="wrapper--global-dog-search">
            <header>
-               <?php echo do_shortcode('[wd_asp id="1"]'); ?>
+	           <?php echo do_shortcode('[wpdreams_ajaxsearchpro_results id="1" element="div"]'); ?>
            </header>
 
            <footer>
-	           <?php echo do_shortcode('[wpdreams_ajaxsearchpro_results id="1" element="div"]'); ?>
+	           <?php echo do_shortcode('[wd_asp id="1"]'); ?>
            </footer>
        </section>
 
